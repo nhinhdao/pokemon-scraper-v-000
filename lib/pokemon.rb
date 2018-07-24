cb73505c103a46a0febd99e71f5435072d0d3ad1
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-24 17:49:59 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,26 +17,23 @@ class Pokemon
     def alter_hp(hpk, database)
         # binding.pry
         database.execute("UPDATE pokemon set hp = ? where name = ?", hpk, self.name)
-        # database.execute("UPDATE pokemon set hp = #{hpk.to_i} where name = '#{self.name}'")
     end
-    # it "alters Pikachu's hp to 59" do
-    #   pikachu.alter_hp(59, @db)
-    #   expect(Pokemon.find(1, @db).hp).to eq(59)
     # database.execute("select * from pokemon")
-    # end
 
     def self.save(name, type, database)
         database.execute("insert into pokemon (name, type) values (?, ?)", name, type)
     end
 
     def self.find(num, database)
-        binding.pry
-        Pokemon.all.find do |pk|
-            if pk.id = num
-             Pokemon.save(pk.name, pk.type, database)
-             end
-         end
-         database.find("Pikachu")
+        a = database.execute("select * from pokemon where id = ?", num)
+        Pokemon.save(a[0][1], a[0][2], database)
+        # binding.pry
+        # Pokemon.all.find do |pk|
+        #     if pk.id = num
+        #      Pokemon.save(pk.name, pk.type, database)
+        #      end
+        #  end
+        #  database.find("Pikachu")
     end
 
     def self.all
