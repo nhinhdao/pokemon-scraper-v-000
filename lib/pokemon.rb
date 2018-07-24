2ed629856e2fb6e1cd182a2804bba7dd45eda03c
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-24 17:59:55 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -27,7 +27,7 @@ class Pokemon
     def self.find(num, database)
         a = database.execute("select name, type from pokemon where id = ?", num)
         Pokemon.save(a[0][0], a[0][1], database)
-        binding.pry
+        # binding.pry
         # Pokemon.all.find do |pk|
         #     if pk.id = num
         #      Pokemon.save(pk.name, pk.type, database)
